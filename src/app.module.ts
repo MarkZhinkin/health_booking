ce86284df63f02dbae9efb3aa1de6950ead0d06f
ZhinkinMark <zhinkin_ma@groupbwt.com>
2022-07-16 20:24:21 +0300

    fet(*): expand modules and add swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { ScheduleModule } from "@nestjs/schedule";
+import { MongooseModule } from "@nestjs/mongoose";
+import createConnectionString from "./commons/utils/create-connection-string";
+import { UsersModule } from "./users/users.module";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: ".env",
+            isGlobal: true,
+        }),
+        MongooseModule.forRoot(createConnectionString()),
+        ScheduleModule.forRoot(),
+        UsersModule,
+        AuthModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
